a56f9153954972410ec9f03e418235da0ac86cf5
Jimmy Jouanne <jimmy.jouanne@gmail.com>
2021-01-09 10:29:00 +0100

    chore: moove logical module in lib directory

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['@nuxtjs', 'plugin:prettier/recommended'],
+  rules: {
+    curly: 'error',
+    // Allow sparse arrays
+    'no-sparse-arrays': 'off',
+    // Allow v-html
+    'vue/no-v-html': 'off'
+  }
+}
